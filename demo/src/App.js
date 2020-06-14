ee6cd9066504c617384c0028b7d715a1c3dab344
Luis Alejo <lou.m.alejo@gmail.com>
2020-06-14 00:25:17 -0400

    running a few tests

--- demo/src/App.js
+++ demo/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>{"I'M LEARNING HERE"}</h1>
+        <h1>{"I'M LEARNING HERE !!"}</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
